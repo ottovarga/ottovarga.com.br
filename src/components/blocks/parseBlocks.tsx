9f7208811f09d0ff59daafb8b5af62d5977e7fa7
Gustavo Rocha <gustavo@onserp.com.br>
2023-08-23 12:32:02 -0300

    add table of contents

--- src/components/blocks/parseBlocks.tsx
+++ src/components/blocks/parseBlocks.tsx
@@ -3,6 +3,7 @@ import { domToReact } from 'html-react-parser'
 import { Element } from 'domhandler/lib/node'
 
 import Code, { getLanguage, getCode } from '@components/blocks/code'
+import TableOfContents from '@components/blocks/tableOfContents'
 
 const parseBlocks = (node: Element) => {
   // code block
@@ -11,6 +12,18 @@ const parseBlocks = (node: Element) => {
       node.children.length > 0 && (
         <Code language={getLanguage(node)}>{domToReact(getCode(node))}</Code>
       )
+    )
+  }
+
+  // table of contents
+  if (node.attribs && node.attribs.id === 'ez-toc-container') {
+    return (
+      <TableOfContents>
+        {domToReact(
+          // @ts-ignore
+          node.children.filter(({ name }) => name === 'nav')
+        )}
+      </TableOfContents>
     )
   }
 }
---
+++ src/components/blocks/tableOfContents.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useCollapse } from 'react-collapsed'
+import Arrow from '@svg/arrow.svg'
+
+export default function TableOfContents({ children }) {
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
+
+  return (
+    <div className="py-4 pl-4 pr-8 border-2 border-gray-200 rounded-lg relative bg-gray-100">
+      <h2
+        className="text-xl font-bold"
+        style={{
+          margin: 0,
+          color: 'rgb(63, 63, 70)'
+        }}
+      >
+        Índice
+      </h2>
+      <button
+        className={`toggle-toc ${isExpanded ? '-rotate-90' : 'rotate-90'}`}
+        {...getToggleProps()}
+      >
+        <Arrow className="w-4 lg:w-6 transform transition-all origin-center duration-300 text-gray-400 fill-current" />
+      </button>
+      <div className="navigation-toc" {...getCollapseProps()}>
+        {children}
+      </div>
+    </div>
+  )
+}
