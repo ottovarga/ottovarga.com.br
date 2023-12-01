c941e2dbf9c811968cf1dce98b5707511e1e7ed9
Gustavo Rocha <gustavo@onserp.com.br>
2023-12-01 12:19:19 -0300

    fix message to slack

--- src/newsletter/formatFeed.ts
+++ src/newsletter/formatFeed.ts
@@ -275,11 +275,10 @@ export const formatFeed: (feed: Feed) => string[] = (feed: Feed) => {
     finalStringArr[index] = ''
     finalStringArr[index] += `Notícia ${index + 1}:\n\n`
     finalStringArr[index] += `Data: ${content.date}\n`
-    finalStringArr[index] += `*${content.title}\n\n${content.content}\n\n`
-    finalStringArr[index] += `*\nCategorias: ${content.categories.join(
-      ', '
-    )}\n\n`
+    finalStringArr[index] += `*${content.title}*\n\n${content.content}\n\n`
+
     finalStringArr[index] += `*Via @${content.feedName}*\n\n`
+    finalStringArr[index] += `Categorias: ${content.categories.join(', ')}\n\n`
     finalStringArr[
       index
     ] += `Link da notícia completa: ${content.link}\n\n-----------\n\n`
